45331de911a25ab57f497c377eb2864a3b436238
Lucas Della Bella <dellabella.lucas@gmail.com>
2019-04-13 11:35:44 -0700

    Switch residency form select element to use material ui

--- src/components/Home/ResidencyForm.js
+++ src/components/Home/ResidencyForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import InputLabel from "@material-ui/core/InputLabel";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Question from '../Common/FormQuestion';
+import Dropdown from '../Common/Dropdown';
 import { FirebaseContext } from '../Firebase';
 import { AuthUserContext } from '../Session';
 import { updateUser } from '../../utils/DBUtils';
@@ -44,12 +46,19 @@ const ResidencyFormInContext = ({ firebase, authUser, onSubmit }) => {
           <Question defaultValue={'no'} groupName="stillInUS">
             Are you still in the US?
         </Question>
-          <p>When was your first visit to the US?</p>
-          <select name="firstVisitToUS">
-            {[...Array(50).keys()].map(i => (
+        <div className="field-with-label">
+          <InputLabel
+            htmlFor='citizenship'
+          >
+            Date of first visit to USA:
+          </InputLabel>
+          <Dropdown
+            id="firstVisitToUS"
+            name="firstVisitToUS"
+            data={[...Array(20).keys()].map(i => (
               <option value={2019 - i}>{2019 - i}</option>
-            ))}
-          </select>
+            ))}/>
+        </div>
         </CardContent>
         <CardActions>
           <Button type="submit" color="primary">
